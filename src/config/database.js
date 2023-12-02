ed2e33262017b53f24684a439824c31ce4cfd61b
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2023-12-02 18:58:20 +0300

    test

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODBCONNECTION, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex:true, useFindAndModify:false
+    useCreateIndex: true, useFindAndModify: false//aa
 })
     .then(() => console.log('veritabanına bağlanıldı'))
     .catch(hata => console.log(`veritabanı baglantı hatası ${hata}`))
